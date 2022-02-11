9f59fb5ac6f5daa125250916f14fd490576ec405
b3rking <ishimweeschylebersy@gmail.com>
2022-02-11 15:31:52 +0200

    fixing the typo

--- index.php
+++ index.php
@@ -41,6 +41,13 @@ don't copy link from playlist not working
 <body>
 	<div class="content">
 		<h2>Daflx Youtube Thumbnails Downloader</h2>
+		<p>
+			usage for the app
+			- copy link from yt
+			- paste and click on fetch thumbnails
+			- right click on the image and select save image as
+			side note : don't copy a link from a playlist use single video
+		</p>
 	<form action="" method="post">
         <div class="form-group">
             <div class="p__svg">
@@ -50,24 +57,24 @@ don't copy link from playlist not working
             <input type="text" class="form-control" placeholder="e.g. https://youtu.be/nb5BHPYbBBY" name="video_link" id="video_link" required>
         </div>
         <div class="mt-3">
-            <button type="submit" class="btn dbtn">Fetch thumbnails</button>
+            <button type="submit" class="btn dbtn">Fetch Thumbnails</button>
         </div>
     </form>
     <?php if (isset($img)) { ?>
     
     <div class="mt-4">
-        <div class="row">
-            <div class="col-12 mb-3">
+        <div class="col-12">
+            <div class="col-6 mb-3">
                 <img src="<?php echo $img["lg"]; ?>" alt="" class="img-fluid">
-                <h5>Max Resulation</h5>
+                <h5>Max Resolution</h5>
             </div>
-            <div class="col-md-6 mb-3">
+            <div class="col-md-3 mb-3">
                 <img src="<?php echo $img["md"]; ?>" alt="" class="img-fluid">
-                <h5>Medium Resulation</h5>
+                <h5>Medium Resolution</h5>
             </div>
-            <div class="col-md-6 mb-3">
+            <div class="col-md-3 mb-3">
                 <img src="<?php echo $img["sm"]; ?>" alt="" class="img-fluid">
-                <h5>Small Resulation</h5>
+                <h5>Small Resolution</h5>
             </div>
         </div>
     </div>
